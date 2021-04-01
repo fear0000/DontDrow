906e5126208a87383de8101f63099b1451e86c15
reptiloyds <hairon20152003@mail.ru>
2021-04-02 02:17:31 +0400

    FirstScene

---
+++ Don't drow/Assets/Scripts/BoadMoving.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoadMoving : MonoBehaviour
+{
+    [SerializeField] int speed;
+    [SerializeField] GameObject player;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        rb.velocity = Vector3.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rb.velocity = Vector3.zero;
+        anim.SetTrigger("isStoped");
+    }
+    public void OnKill()
+    {
+        var playerRb = player.GetComponent<Rigidbody2D>();
+        playerRb.velocity = Vector3.down * 4.5f;
+    }
+}
---
+++ Don't drow/Assets/Scripts/PlayerScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScene : MonoBehaviour
+{
+    private Animator anim;
+    private void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        anim.SetTrigger("isFloatUp");
+        var playerRb = gameObject.GetComponent<Rigidbody2D>();
+        playerRb.velocity = Vector3.down * 1.7f;
+    }
+}
