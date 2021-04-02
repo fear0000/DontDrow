1404999dea17a2f3492fb63ea41c021161971b34
fear0000 <57982606+fear0000@users.noreply.github.com>
2021-04-02 17:18:53 +0300

    Сделол анимации

---
+++ Don't drow/Assets/Scripts/Fishman.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fishman : MonoBehaviour
+{
+    [SerializeField] private Animator bubbleAnim;
+    [SerializeField] private Animator characterAnim;
+
+    public void MakeBubble()
+    {
+        characterAnim.SetTrigger("MakeBubble");
+        bubbleAnim.SetTrigger("MakeBubble");
+    }
+}
