51b2bcdbea342d6f9e8a79880b9fd46d15b28fcb
reptiloyds <hairon20152003@mail.ru>
2021-04-02 13:12:09 +0400

    Sound

--- Don't drow/Assets/Scripts/BoadMoving.cs
+++ Don't drow/Assets/Scripts/BoadMoving.cs
@@ -6,12 +6,14 @@ public class BoadMoving : MonoBehaviour
 {
     [SerializeField] int speed;
     [SerializeField] GameObject player;
+    private AudioSource audioSorce;
     private Rigidbody2D rb;
     private Animator anim;
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
+        audioSorce = gameObject.GetComponent<AudioSource>();
     }
     private void Start()
     {
@@ -21,10 +23,11 @@ public class BoadMoving : MonoBehaviour
     {
         rb.velocity = Vector3.zero;
         anim.SetTrigger("isStoped");
+        audioSorce.Stop();
     }
     public void OnKill()
     {
         var playerRb = player.GetComponent<Rigidbody2D>();
-        playerRb.velocity = Vector3.down * 4.5f;
+        playerRb.velocity = Vector3.down * 5.5f;
     }
 }
--- Don't drow/Assets/Scripts/PlayerScene.cs
+++ Don't drow/Assets/Scripts/PlayerScene.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerScene : MonoBehaviour
 {
     private Animator anim;
+    private AudioSource audioSorce;
     private void Awake()
     {
         anim = gameObject.GetComponent<Animator>();
+        audioSorce = gameObject.GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        anim.SetTrigger("isFloatUp");
+        audioSorce.Play();
+        anim.SetTrigger("isDrow");
         var playerRb = gameObject.GetComponent<Rigidbody2D>();
         playerRb.velocity = Vector3.down * 1.7f;
     }
