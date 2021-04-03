6f483971f71151353944688355f10b57ecf1f86e
reptiloyds <hairon20152003@mail.ru>
2021-04-03 23:49:58 +0400

    в

---
+++ Don't drow/Assets/Scripts/2Level/Button.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private Sprite[] onOffButton;
+    private SpriteRenderer sr;
+    public event EventHandler<ChangeEventArgs> Change;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        sr.sprite = onOffButton[1];
+        var changeEventArgs = new ChangeEventArgs { isPressed = true };
+        Change?.Invoke(this, changeEventArgs);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        sr.sprite = onOffButton[0];
+        var changeEventArgs = new ChangeEventArgs { isPressed = false };
+        Change?.Invoke(this, changeEventArgs);
+    }
+}
+public class ChangeEventArgs : EventArgs
+{
+    public bool isPressed { get; set; }
+}
---
+++ Don't drow/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class DialogManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private Animator dialogueAnim;
+    public event Action EndDialogue;
+    private bool isRooted;
+    //Исправить говно
+    public Animator fishAnim;
+    public Animator characterAnim;
+    public Rigidbody2D character;
+    //
+    private Queue<string> sentences;
+    private void Start()
+    {
+        sentences = new Queue<string>();
+        isRooted = true;
+    }
+
+    public void StartDiologue(Diologue diologue)
+    {
+        dialogueAnim.SetTrigger("Open");
+        nameText.text = diologue.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in diologue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDiologue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char symbol in sentence.ToCharArray())
+        {
+            dialogueText.text += symbol;
+            yield return new WaitForSeconds(0.03f);
+        }
+    }
+    public void EndDiologue()
+    {
+        dialogueAnim.SetTrigger("Close");
+        if(characterAnim != null)
+        {
+            characterAnim.enabled = true;
+            character.constraints = RigidbodyConstraints2D.None;
+            character.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+        if (isRooted && characterAnim != null)
+        {
+            fishAnim.SetTrigger("MakeBubble");
+            characterAnim.SetTrigger("MakeBubble");
+            isRooted = !isRooted;
+        }
+        EndDialogue?.Invoke();
+    }
+}
