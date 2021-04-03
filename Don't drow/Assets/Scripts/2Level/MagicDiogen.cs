7dd14a8b7ab441f9ac440eac83af579913f44d06
reptiloyds <hairon20152003@mail.ru>
2021-04-03 23:22:09 +0400

    ddd

--- Don't drow/Assets/Scripts/2Level/MagicDiogen.cs
+++ Don't drow/Assets/Scripts/2Level/MagicDiogen.cs
@@ -4,16 +4,59 @@ using UnityEngine;
 
 public class MagicDiogen : MonoBehaviour
 {
-    [SerializeField] private Diologue startsDiologue;
-    [SerializeField] private Diologue constDiologue;
+    [SerializeField] private DialogManager dm;
+    [SerializeField] private Diologue startDiologue;
+    [SerializeField] private Diologue constantDiologue;
     [SerializeField] private Diologue helpDiologue;
-    void Start()
+    [SerializeField] private Animator eAnim;
+    private bool isUsable;
+    private bool firstDialog;
+    private bool isTalking;
+    private void Start()
     {
-        
+        dm.EndDialogue += OnEndDialogue;
+        firstDialog = false;
+        isTalking = false;
+    }
+    private void OnEndDialogue()
+    {
+        isTalking = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Open");
+            isUsable = true;
+            eAnim.SetTrigger("Open");
+        }
     }
 
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Close");
+            isUsable = false;
+            eAnim.SetTrigger("Close");
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && isUsable && !isTalking)
+        {
+            Debug.Log(1);
+            isTalking = true;
+            if (!firstDialog)
+            {
+                Debug.Log(2);
+                dm.StartDiologue(startDiologue);
+                firstDialog = true;
+            }
+            else
+            {
+                dm.StartDiologue(constantDiologue);
+            }
+        }
     }
 }
