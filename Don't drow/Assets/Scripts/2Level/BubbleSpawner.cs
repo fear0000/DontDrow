203d6d8c80536ea6133955ec04c0f5303d66b3f8
reptiloyds <hairon20152003@mail.ru>
2021-04-04 20:09:55 +0400

    в

--- Don't drow/Assets/Scripts/2Level/BubbleSpawner.cs
+++ Don't drow/Assets/Scripts/2Level/BubbleSpawner.cs
@@ -66,7 +66,7 @@ public class BubbleSpawner : MonoBehaviour
     }
     private IEnumerator Shot()
     {
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(5f);
         isActive = !isActive;
         StartCoroutine(Reload());
     }
